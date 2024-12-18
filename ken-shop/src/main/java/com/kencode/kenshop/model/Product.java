bb97d8ada56f4e1b054d16697b9fbeefb19af73d
iiiamken <kenny.t.dinh93@gmail.com>
2024-12-18 21:13:56 +0100

    created product class

---
+++ ken-shop/src/main/java/com/kencode/kenshop/model/Product.java
@@ -0,0 +1,12 @@
+package com.kencode.kenshop.model;
+
+import java.math.BigDecimal;
+
+public class Product {
+    private Long id;
+    private String name;
+    private String brand;
+    private String description;
+    private BigDecimal price;
+    private int inventory;
+}
