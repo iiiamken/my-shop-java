2beae20127475a9ae2b1a9d833b2968ec24992b1
iiiamken <kenny.t.dinh93@gmail.com>
2024-12-18 21:15:13 +0100

    added Category and Image

--- ken-shop/src/main/java/com/kencode/kenshop/model/Product.java
+++ ken-shop/src/main/java/com/kencode/kenshop/model/Product.java
@@ -1,12 +1,17 @@
 package com.kencode.kenshop.model;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 public class Product {
     private Long id;
     private String name;
     private String brand;
-    private String description;
     private BigDecimal price;
     private int inventory;
+    private String description;
+
+    private Category category;
+
+    private List<Image> images;
 }
